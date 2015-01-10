395ed2526d29999abec49e0fbdb48d97a786ba77
Cyquine <cyquine@github.com>
2015-01-10 15:30:51 +0000

    added loading icons

--- live_comments.js
+++ live_comments.js
@@ -3,12 +3,16 @@ function div(a, b) {return (a-a%b)/b} // integer division
 var LAST_CALL = 0,
     NEXT_CALL_TIMEOUT = setTimeout(undefined);
 
-document.getElementById('more').onclick = list;
-
 function getComments() {
     var script = document.createElement('script');
+<<<<<<< HEAD
+    script.setAttribute('src',
+         'https://www.reddit.com/r/mlplounge/comments.json?jsonp=more&before=' +
+                                                            getComments.before);
+=======
     script.src = 'https://www.reddit.com/r/mlplounge/comments.json?jsonp=more&before='
                  + getComments.before;
+>>>>>>> eb6e54a69af3d0e0fa9a015e0e43a1bbe143b216
     document.body.appendChild(script);
     LAST_CALL = Date.now();
     document.body.removeChild(script);
@@ -20,6 +24,10 @@ function more(data) {
     new_comments = Array.prototype.unshift.apply(list.comments,
                                                       data['data']['children']);
     if (document.getElementById('content').childNodes.length === 0) {
+<<<<<<< HEAD
+        getComments.before = list.comments[0]['data']['name'];
+=======
+>>>>>>> eb6e54a69af3d0e0fa9a015e0e43a1bbe143b216
         list();
     } else if (new_comments > 0) {
         getComments.before = list.comments[0]['data']['name'];
@@ -32,11 +40,17 @@ function more(data) {
 }
 
 function list() {
+    var comments = list.comments;
+    list.comments = {};
     document.getElementById('more').className = 'fadedout';
+<<<<<<< HEAD
+=======
     var comments = list.comments;
     list.comments = {};
     var parent = document.createDocumentFragment();
+>>>>>>> eb6e54a69af3d0e0fa9a015e0e43a1bbe143b216
 
+    var parent = document.createDocumentFragment();
     for (var i = 0; i < comments.length; i++) {
         var c = comments[i]['data'];            
 
@@ -46,21 +60,21 @@ function list() {
         link_title.setAttribute('href', c['link_url']);
 
         var link_author = document.createElement('a');
-        link_author.className = 'author';
         link_author.appendChild(document.createTextNode(c['link_author']));
+        link_author.setAttribute('class', 'author');
         link_author.setAttribute('target', '_blank');
         link_author.setAttribute('href', 'https://www.reddit.com/u/' +
                                                               c['link_author']);
 
         var link = document.createElement('p');
-        link.className = 'link';
         link.appendChild(link_title);
         link.appendChild(document.createTextNode(' by '));
         link.appendChild(link_author);
+        link.setAttribute('class', 'link');
 
         var author = document.createElement('a');
-        author.className = 'author';
         author.appendChild(document.createTextNode(c['author']));
+        author.setAttribute('class', 'author');
         author.setAttribute('target', '_blank');
         author.setAttribute('href', 'https://www.reddit.com/u/' + c['author']);
         
@@ -69,48 +83,51 @@ function list() {
 
         var created = document.createElement('time'),
             created_time = new Date(c['created_utc']*1000);
-        created.className = 'created';
         created.appendChild(created_text);
+        created.setAttribute('class', 'created');
         created.setAttribute('datetime', created_time.toISOString());
-        created.setAttribute('title', created_time.toString());        
+        created.setAttribute('title', created_time.toString());
 
         var load_submission = document.createElement('button');
-        load_submission.className = 'load';
-        // load_submission.setAttribute('onclick');
+        add_loader(load_submission);
+        load_submission.setAttribute('class', 'load');
+        load_submission.setAttribute('onclick', 'fetch(this)');
 
-        var last_comment = document.createElement('button');
-        last_comment.className = 'load';
+        var load_parent = document.createElement('button');
+        add_loader(load_parent);
+        load_parent.setAttribute('class', 'load parent');
+        load_parent.setAttribute('onclick', 'fetch_parent(this)');
 
         var permalink = document.createElement('a');
-        permalink.appendChild(document.createTextNode('r'));
+        permalink.setAttribute('class', 'permalink');
         permalink.setAttribute('target', '_blank');
         permalink.setAttribute('href',
                     ['https://www.reddit.com/r/MLPLounge/comments', 
                     c['link_id'].slice(3), c['link_title'], c['id']].join('/'));
-        permalink.className = 'load';
 
         var comment_header = document.createElement('div');
-        comment_header.appendChild(last_comment);
+        comment_header.appendChild(load_parent);
         comment_header.appendChild(permalink);
         comment_header.appendChild(author);
         comment_header.appendChild(created);
-        comment_header.className = 'comment-header';
+        comment_header.setAttribute('class', 'comment-header');
 
         var comment = document.createElement('div');
         comment.innerHTML = c['body_html'];
         comment.innerHTML = comment.firstChild.nodeValue;
         comment.insertBefore(comment_header, comment.firstChild);
-        comment.className = 'comment';
+        comment.setAttribute('class', 'comment');
         
         var submission = document.createElement('div');
         submission.appendChild(load_submission);
         submission.appendChild(link);
         submission.appendChild(comment);
-        submission.className = 'submission fadedout';
+        submission.setAttribute('class', 'submission fadedout');
+
         parent.appendChild(submission);
 
         setTimeout(function (el) {el.className = 'submission fadedin'},
-            50*(Math.min(comments.length, 3) - i), submission);
+            100*(Math.min(comments.length, 3) - i), submission);
     }
 
     var wrapper = document.getElementById('content');
@@ -135,4 +152,24 @@ function timestamp(el, created) { // sets live timestamp, then sleeps again
         el.nodeValue = 'just now';
         setTimeout(timestamp, 60000 - age, el, created);
     }    
+}
+
+function add_loader(el) {
+    ['tl', 'tr', 'br', 'bl'].forEach(function(which) {
+        var segment = document.createElement('span');
+        segment.setAttribute('class', 'load-segment load-' + which);
+        el.appendChild(segment);
+    });
+}
+
+function fetch(el) {
+    for (var i = 0; i < el.children.length; i++) {
+        el.children[i].className += ' loading';
+    }
+}
+
+function fetch_parent(el) {
+    for (var i = 0; i < el.children.length; i++) {
+        el.children[i].className += ' loading';
+    }
 }

454cf81612fd014cfc1ec1c7b25a9d39affa2a51
Cyquine <cyberquine@gmail.com>
2015-01-24 17:25:07 +0000

    Rewrote bpm.js

--- bpm.js
+++ bpm.js
@@ -1,52 +1,31 @@
 'use strict';
 liveComments.bpm = function(node) { // borrows heavily from bpm's mechanism
-    node = node.firstChild;
-    var depth = 1;
-    while (true) {
-        if (node.nodeType === 3) { // Node.TEXT_NODE == 3, signifies escaped emote
-            var matches = node.data.split(liveComments.bpm.emoteRegexp);
-            if (matches.length > 1) { // splits emote over 2 text nodes
-                node.data = ')' + matches.pop();
-                var parent = node.parentNode;
-                parent.insertBefore(document.createTextNode(
-                                  matches.shift() + matches.shift()), node);
-                while (matches.length > 0) {
-                    parent.insertBefore(document.createTextNode(')' +
-                                  matches.shift() + matches.shift()), node);
-                }
-            }
-        } else if (node.tagName === 'A') { // an anchor having no text is a
-                                           // good sign of it being an emote
-
-            if (node.hasChildNodes()) {
-                node.target = '_blank';
-                if (node.host === window.location.host) {
-                    node.href = '//reddit.com' + node.getAttribute('href');
-                }
-            } else {
-                var text = '[](' + node.getAttribute('href');
-                text += node.hasAttribute('title') ? ' "' + node.title + '")' : ')';
-
-                var replacement = document.createElement('span');
-                replacement.appendChild(document.createTextNode(text));
-
-                var parent = node.parentNode;
-                parent.insertBefore(replacement, node);
-                parent.removeChild(node);
-                node = replacement.firstChild;
+    for (var textNodeWalker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT),
+             textNode; textNode = textNodeWalker.nextNode();) {
+        var matches = textNode.data.split(liveComments.bpm.emoteRegexp);
+        if (matches.length > 1) { // split emote over 2 text nodes
+            textNode.data = ')' + matches.pop();
+            var parent = textNode.parentNode;
+            parent.insertBefore(document.createTextNode(
+                                  matches.shift() + matches.shift()), textNode);
+            while (matches.length > 0) {
+                parent.insertBefore(document.createTextNode(')' +
+                                  matches.shift() + matches.shift()), textNode);
             }
         }
+    }
 
-        if (node.hasChildNodes()) {
-            node = node.firstChild;
-            depth++;
-            continue;
-        } else {
-            while (!node.nextSibling) {
-                node = node.parentNode;
-                if (--depth) return;
+    for (var links = node.getElementsByTagName('a'), link, i = 0;
+                                       i < links.length, link = links[i]; i++) {
+        if (link.hasChildNodes()) {
+            link.target = '_blank';
+            if (link.host === window.location.host) {
+                link.href = '//reddit.com' + link.pathname;
             }
-            node = node.nextSibling;
+        } else { // an anchor having no text is a good sign of it being an emote
+            link.parentNode.replaceChild(document.createTextNode('[](' +
+                            link.pathname + (link.hasAttribute('title') ? ' "' +
+                                               link.title + '")' : ')')), link);
         }
     }
 };
